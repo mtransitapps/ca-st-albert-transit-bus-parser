8537fc0ef1c6196cd739d4fd6dd9f0ef21d61034
Mathieu Méa <mathieu.mea@gmail.com>
2018-09-01 08:37:50 -0400

    New URL

--- src/org/mtransit/parser/ca_st_albert_transit_bus/StAlbertTransitBusAgencyTools.java
+++ src/org/mtransit/parser/ca_st_albert_transit_bus/StAlbertTransitBusAgencyTools.java
@@ -30,7 +30,7 @@ import org.mtransit.parser.mt.data.MTrip;
 import org.mtransit.parser.mt.data.MTripStop;
 
 // https://stalbert.ca/city/transit/rider-tools/open-data-gtfs/
-// https://stalbert.ca/uploads/files-zip/google_transit2.zip
+// https://stalbert.ca/uploads/files-zip/google_transit..zip
 public class StAlbertTransitBusAgencyTools extends DefaultAgencyTools {
 
 	public static void main(String[] args) {
@@ -731,9 +731,6 @@ public class StAlbertTransitBusAgencyTools extends DefaultAgencyTools {
 				mTrip.setHeadsignString("Rodeo", mTrip.getHeadsignId());
 				return true;
 			}
-		}
-		if (isGoodEnoughAccepted()) {
-			return super.mergeHeadsign(mTrip, mTripToMerge);
 		}
 		System.out.printf("\nUnexpected trips to merge %s & %s!\n", mTrip, mTripToMerge);
 		System.exit(-1);
